9bb124fd8fdeaa0fdaf674f87831114132aebea8
prabinlamsal19 <lamsalprabin74@gmail.com>
2025-02-11 11:44:06 +0545

    feat: nodemon , bind mounts , envs config

--- apps/node-app/index.js
+++ apps/node-app/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+// import Pool from 'pg';
 const app = express();
 
 app.get('/node-app', (req, res) => {
@@ -9,3 +10,15 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port number ${PORT}`);
 });
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// pool.query('SELECT NOW()', (err, res) => {
+//   if (err) {
+//     console.error('Database connection error:', err);
+//   } else {
+//     console.log('Database connected:', res.rows[0]);
+//   }
+// });
