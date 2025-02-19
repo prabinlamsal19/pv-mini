25b6b55602d00cd89845db13753dfd86df5227fb
prabinlamsal19 <lamsalprabin74@gmail.com>
2025-02-19 14:18:39 +0545

    fix: added db-nest as the db host

--- apps/nest-app/src/app.module.ts
+++ apps/nest-app/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db-nest',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'mysecretpassword',
+      database: process.env.POSTGRES_DB || 'nestDatabase',
+      entities: [__dirname + '/**/entities/*{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
